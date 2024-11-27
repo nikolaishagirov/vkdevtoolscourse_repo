9fba8e914a5dc04d669855e70c2d220733cbb7e3
Nikolai Shagirov <nikkey.shagirov@yandex.ru>
2024-11-27 16:17:35 +0300

    print messages about program start and finish

--- example.cpp
+++ example.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 
 int main() {
+    std::cout<<"The program is started...\n";
     for(int i=0; i<10; i++)
         std::cout<<"Hello world!\n";
+    std::cout<<"The program is finished\n";
     return 0;
 }
