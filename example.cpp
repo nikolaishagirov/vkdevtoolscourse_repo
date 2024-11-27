92bfec09bd678b69dce022d93e0488b50277b260
Nikolai Shagirov <nikkey.shagirov@yandex.ru>
2024-11-27 16:19:03 +0300

    print 'VK is a good company'

--- example.cpp
+++ example.cpp
@@ -4,6 +4,7 @@ int main() {
     std::cout<<"The program is started...\n";
     for(int i=0; i<10; i++)
         std::cout<<"Hello world!\n";
+    std::cout<<"VK is a good company\n";
     std::cout<<"The program is finished\n";
     return 0;
 }
